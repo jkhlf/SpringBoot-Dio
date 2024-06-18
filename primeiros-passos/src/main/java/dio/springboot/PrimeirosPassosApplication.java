cfd9891ef8d6201959b9750cd53882dd4a9ec00a
Joao Khalaf <joao.khalaf@gmail.com>
2024-06-18 15:50:52 -0300

    Atribuindo properties value usando aplication.properties

--- primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
+++ primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -2,7 +2,6 @@ package dio.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class PrimeirosPassosApplication {
